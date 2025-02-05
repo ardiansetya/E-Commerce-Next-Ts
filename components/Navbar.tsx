ef7ca83f8db5a6e6f1a864fef6e22cfb6f11f97a
ardiansetya <ardiansetya129@gmail.com>
2025-02-05 21:03:53 +0700

    create store switcher with open modal

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,12 +2,26 @@ import { UserButton } from "@clerk/nextjs";
 import React from "react";
 import { MainNav } from "./MainNav";
 import StoreSwitcher from "./StoreSwitcher";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import db from "@/lib/db";
 
-const Navbar = () => {
+const Navbar = async() => {
+  const {userId} = await auth()
+  
+  if (!userId) {
+    redirect("/sign-in")
+  }
+
+  const stores = await db.store.findMany({
+    where: {
+      userId
+    }
+  })
   return (
     <div className="border-b">
       <div className="flex h-16 items-center px-4 ">
-        <StoreSwitcher/>
+        <StoreSwitcher items={stores} />
         <MainNav className="mx-6"/>
         <div className="ml-auto flex items-center space-x-4">
           <UserButton />
--- components/StoreSwitcher.tsx
+++ components/StoreSwitcher.tsx
@@ -6,9 +6,17 @@ import { Store } from "@prisma/client";
 import { useStoreModal } from "@/hooks/useStoreModal";
 import { useParams, useRouter } from "next/navigation";
 import { Button } from "./ui/button";
-import { ChevronDown, StoreIcon } from "lucide-react";
+import { Check, ChevronDown, PlusCircle, StoreIcon } from "lucide-react";
 import { cn } from "@/lib/utils";
-import { Command, CommandInput, CommandList } from "./ui/command";
+import {
+  Command,
+  CommandEmpty,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+} from "./ui/command";
+import { CommandGroup } from "cmdk";
 
 type PopoverTriggerProps = React.ComponentPropsWithoutRef<
   typeof PopoverTrigger
@@ -49,14 +57,48 @@ const StoreSwitcher = ({ className, items = [] }: StoreSwitcherProps) => {
           aria-expanded={open}
           size={"sm"}>
           <StoreIcon className="w-4 h-4 mr-2" />
-          Current Store
+          {currentStore?.label}
           <ChevronDown className="w-4 h-4 shrink-0 opacity-50 ml-auto" />
         </Button>
       </PopoverTrigger>
       <PopoverContent className="w-[200px] p-0">
         <Command>
           <CommandList>
-            <CommandInput placeholder="Search store..."/>
+            <CommandInput placeholder="Search store..." />
+            <CommandEmpty>No store found.</CommandEmpty>
+            <CommandGroup  heading="Stores">
+              {formattedItems.map((store) => (
+                <CommandItem
+                  key={store.value}
+                  onSelect={() => onStoreSelect(store)}
+                  className="text-sm">
+                  <StoreIcon className="w-4 h-4" />
+                  {store.label}
+                  <Check
+                    className={cn(
+                      "ml-auto h-4 w-4",
+                      currentStore?.value === store.value
+                        ? "opacity-100"
+                        : "opacity-0"
+                    )}
+                  />
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+          <CommandSeparator />
+          <CommandList>
+            <CommandGroup>
+              <CommandItem
+                onSelect={() => {
+                  setOpen(false);
+                  storeModal.onOpen();
+                }}
+                className="">
+                <PlusCircle className="mr-2 w-5 h-5" />
+                Create Store
+              </CommandItem>
+            </CommandGroup>
           </CommandList>
         </Command>
       </PopoverContent>
